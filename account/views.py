a1ebb834bfb9aeed55ed9a3246a07c65496c8f75
o4x <8reza0@gmail.com>
2023-12-06 19:57:16 +0330

    Add login and signup views

--- account/views.py
+++ account/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
 
-# Create your views here.
+from .models import User
+
+
+def login_view(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            # messages.success(request, "Login successful.")
+            return redirect("home") 
+        else:
+            return render(request, "login.html", {"error": "نام کاربری یا رمز عبور اشتباه است"})
+    return render(request, "login.html") 
+
+def signup_view(request):
+    if request.method == "POST":
+        first_name = request.POST["first_name"]
+        last_name = request.POST["last_name"]
+        email = request.POST["email"]
+        username = request.POST["username"]
+        password = request.POST["password1"]
+        password2 = request.POST["password2"]
+        if password == password2:
+            user = User.objects.create_user(
+                first_name=first_name,
+                last_name=last_name,
+                email=email,
+                username=username,
+                password=password
+            )
+            login(request, user)
+            return redirect("home")
+        else:
+            return render(request, "signup.html", {"error": "رمز عبور و تکرار آن یکسان نیستند"})
+    return render(request, "signup.html")
+
